b1d41e4ddd0ed7ea88099a13d4b4cb261f9b49cc
uwanni <upekshawanniarachchi97@gmail.com>
2023-05-28 14:48:42 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
+import Menu from './pages/Menu';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';      //to add different pages
 
 function App() { 
@@ -10,7 +14,11 @@ function App() {
         <Navbar />
         <Routes> 
           <Route path="/" exact element={<Home />} />
-        </Routes>      
+          <Route path="/menu" exact element={<Menu />} />
+          <Route path="/about" exact element={<About />} />
+          <Route path="/contact" exact element={<Contact />} />
+        </Routes>
+        <Footer />      
       </Router>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../assests/pizzaLogo.png'
 import {Link} from "react-router-dom"
 import ReorderIcon from '@mui/icons-material/Reorder'; 
@@ -6,17 +6,30 @@ import "../styles/Navbar.css"
 
 
 function Navbar() {
+
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+
   return (
     <div className='navbar'>
-      <div className='leftSide'>
-        <img src={Logo} />
+      <div className='leftSide' id={openLinks ? "open" : "close"}>
+        <Link to="/"> <img src={Logo}  /> </Link>
+        <div className='hiddenLinks'>
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
       </div>
       <div className='rightSide'>
         <Link to="/">Home</Link>
         <Link to="/menu">Menu</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
-        <button>
+        <button onClick={toggleNavbar}>
           <ReorderIcon />
         </button>
       </div>      
